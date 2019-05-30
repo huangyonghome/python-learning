a5de224866f16eaacc3dfd5b7f52d09e509e8d6b
huangyong <7865227@qq.com>
2019-05-31 00:05:13 +0800

    update

--- Python-ObjectClass/python类.py
+++ Python-ObjectClass/python类.py
@@ -6,3 +6,27 @@
 
 
 #类的格式
+
+#class 类名:
+#    pass
+
+#举个例子
+
+#创建一个类
+class Person():
+    #构造函数,初始化属性
+
+    def __init__(self,name):
+        self.name = name
+
+    #动态方法
+
+    def eat(self):
+        print("I am eating")
+
+
+
+#实例化一个对象
+
+p = Person('jesse')
+
