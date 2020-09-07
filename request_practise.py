928f0b72595d8005a46de2123bbc642df2eb359f
huangyong <7865227@qq.com>
2020-09-07 22:42:54 +0800

    update

---
+++ request_practise.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-09-07 21:56
+# @Author  : jesse
+# @File    : request_practise.py
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+url = 'http://172.16.20.101:9200/_cat/health'
+r=requests.get(url,auth=HTTPBasicAuth('elastic','Ld73vPze2c'))
+print(r.status_code)
+print(r.text)
+
+print(r.text.split())
